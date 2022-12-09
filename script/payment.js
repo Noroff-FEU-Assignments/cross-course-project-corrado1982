c61b74f2c29909f8298cf524d15d54c3cf69b389
corrado1982 <104769882+corrado1982@users.noreply.github.com>
2022-12-09 12:30:39 +0100

    prova form

---
+++ script/payment.js
@@ -0,0 +1,10 @@
+// console.log("ciao");
+
+// const fullName = document.querySelector("#full-name");
+
+// function validateForm() {
+//   if (fullName.value.length < 10) {
+//     console.log("write your name!");
+//   }
+// }
+// validateForm();
