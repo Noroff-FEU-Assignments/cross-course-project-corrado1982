529a08ff0d30235dbde1fa151a635669405d9aea
corrado1982 <104769882+corrado1982@users.noreply.github.com>
2022-12-11 22:42:12 +0100

    adjusting

--- script/payment.js
+++ script/payment.js
@@ -1,10 +1,22 @@
-// console.log("ciao");
+console.log("ciao");
+const name = document.querySelector("#full-name");
+const email = document.querySelector("#email");
+const address = document.querySelector("#address");
+const addNumber = document.querySelector("#address-number");
+const city = document.querySelector("#city");
+const zipCode = document.querySelector("#zip-code");
+const country = document.querySelector("#country");
 
-// const fullName = document.querySelector("#full-name");
+const cardHolder = document.querySelector("#cardholder");
+const cardNumber = document.querySelector("#card-number");
+const expire = document.querySelector("#expire");
+const cvv = document.querySelector("#cvv");
 
-// function validateForm() {
-//   if (fullName.value.length < 10) {
-//     console.log("write your name!");
-//   }
-// }
-// validateForm();
+const fullName = document.querySelector("#full-name");
+
+function validateForm() {
+  if (fullName.value.length < 10) {
+    console.log("write your name!");
+  }
+}
+validateForm();
