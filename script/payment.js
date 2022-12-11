bff5c55e4a6f1ec8237f3b06a02c0719f3a4ac79
corrado1982 <104769882+corrado1982@users.noreply.github.com>
2022-12-11 23:17:00 +0100

    w i p

--- script/payment.js
+++ script/payment.js
@@ -1,4 +1,4 @@
-console.log("ciao");
+const submit = document.querySelector(".buy-button");
 const name = document.querySelector("#full-name");
 const email = document.querySelector("#email");
 const address = document.querySelector("#address");
@@ -14,9 +14,15 @@ const cvv = document.querySelector("#cvv");
 
 const fullName = document.querySelector("#full-name");
 
-function validateForm() {
+// function validateForm() {
+//   if (fullName.value.length < 10) {
+//     console.log("write your name!");
+//   }
+// }
+// validateForm();
+
+submit.onclick = function () {
   if (fullName.value.length < 10) {
     console.log("write your name!");
   }
-}
-validateForm();
+};
