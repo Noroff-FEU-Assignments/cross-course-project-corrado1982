9b46c79c244a44528262d59ae2a4922ffd753d09
corrado1982 <104769882+corrado1982@users.noreply.github.com>
2022-12-15 10:09:16 +0100

    wip

---
+++ script/items.js
@@ -0,0 +1,14 @@
+const games = {
+  title: "Furious",
+  image: "images/games img/cover-furious.jpg",
+  pXboxOne: true,
+  pXbox: true,
+  pPs4: true,
+  pPs5: true,
+  pNintendo: true,
+  pPc: true,
+  newPrice: 16,
+  usedPrice: 9,
+  digitalPrice: 10,
+  description: "",
+};
