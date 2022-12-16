c43128623aea15c522924bcf65adefc7d655fa10
corrado1982 <104769882+corrado1982@users.noreply.github.com>
2022-12-16 16:49:41 +0100

    wip

--- script/cart.js
+++ script/cart.js
@@ -1,6 +1,9 @@
 import { games } from "./array-games.js";
 
 const leftCart = document.querySelector(".left-cart");
+const price = document.querySelector(".cart-price");
+const subTotatl = document.querySelector(".cart-subtotal");
+const total = document.querySelector(".cart-total");
 
 const queryString = document.location.search;
 const params = new URLSearchParams(queryString);
@@ -12,5 +15,8 @@ console.log("ciao");
 leftCart.innerHTML = `
 <h3>${games[index].title}</h3>
 <img class="cart-image" src="${games[index].image}" alt="image of${games[index].title}" class="item-img">
-
 `;
+
+// price.innerHTML = `<p class="p-plus">${games[index].newPrice}</p>`;
+// subTotatl.innerHTML = `<p">${games[index].newPrice}</p>`;
+// total.innerHTML = `<p">${games[index].newPrice}</p>`;
--- script/detail.js
+++ script/detail.js
@@ -6,12 +6,22 @@ const addToCart = document.querySelector("#add-cart_detailpage");
 const queryString = document.location.search;
 const params = new URLSearchParams(queryString);
 const id = params.get("id");
-const index = id - 1;
+// const index = id - 1;
 
 console.log(id);
 
-detailLeft.innerHTML = `<img class="product-page_image" src="${games[index].image}" alt="image of${games[index].title}" class="item-img">
-<h3>${games[index].title}</h3>
-<p class="game-description">${games[index].description}</p>
-`;
-addToCart.innerHTML = `<a href="cart.html?id=${games[index].id}" class="cta buy-button">Add to cart</a>`;
+for (let i = 0; i < games.length; i++) {
+  if (games[i].id == id) {
+    detailLeft.innerHTML = `<img class="product-page_image" src="${games[i].image}" alt="image of${games[i].title}" class="item-img">
+        <h3>${games[i].title}</h3>
+        <p class="game-description">${games[i].description}</p>
+        `;
+    addToCart.innerHTML = `<a href="cart.html?id=${games[i].id}" class="cta buy-button">Add to cart</a>`;
+  }
+}
+
+// detailLeft.innerHTML = `<img class="product-page_image" src="${games[index].image}" alt="image of${games[index].title}" class="item-img">
+// <h3>${games[index].title}</h3>
+// <p class="game-description">${games[index].description}</p>
+// `;
+// addToCart.innerHTML = `<a href="cart.html?id=${games[i].id}" class="cta buy-button">Add to cart</a>`;
