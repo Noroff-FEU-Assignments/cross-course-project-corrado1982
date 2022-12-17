2b60e6e5b51d702726b5cfb3eb98bca8ca999836
corrado1982 <104769882+corrado1982@users.noreply.github.com>
2022-12-17 23:39:47 +0100

    wip

--- script/cart.js
+++ script/cart.js
@@ -6,6 +6,9 @@ const subTotatl = document.querySelector(".cart-subtotal");
 const total = document.querySelector(".cart-total");
 const removeItem = document.querySelector(".remove-item");
 const removeAll = document.querySelector(".cart-filled");
+const cartIcon = document.querySelector(".cart-icon");
+const empltyMessage = document.querySelector(".empty-message");
+
 // const queryString = document.location.search;
 // const params = new URLSearchParams(queryString);
 // const id = params.get("id");
@@ -20,6 +23,13 @@ const removeAll = document.querySelector(".cart-filled");
 const gamesInSorage = localStorage.getItem("foundGame");
 const parsedSrorage = JSON.parse(gamesInSorage);
 
+if (parsedSrorage === null) {
+  removeAll.style.display = "none";
+  empltyMessage.style.display = "block";
+} else {
+  empltyMessage.style.display = "none";
+}
+
 leftCart.innerHTML += `
 <h3>${parsedSrorage.title}</h3>
 <img class="cart-image" src="${parsedSrorage.image}" alt="image of${parsedSrorage.title}" class="item-img">
@@ -27,10 +37,10 @@ leftCart.innerHTML += `
 removeItem.onclick = function () {
   leftCart.innerHTML = "";
   removeAll.innerHTML = "";
-  removeAll.innerHTML = `<h2>Your cart is empty!</h2>`;
+  removeAll.innerHTML = `<h2 class="empty-message">Your cart is empty!</h2>`;
   localStorage.removeItem("foundGame");
 };
-
+console.log(parsedSrorage);
 // leftCart.innerHTML += `
 // <h3>${foundGame.title}</h3>
 // <img class="cart-image" src="${foundGame.image}" alt="image of${foundGame.title}" class="item-img">
