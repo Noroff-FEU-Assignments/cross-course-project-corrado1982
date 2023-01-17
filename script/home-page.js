398cf5efb78df09480a22b767e3a2e96c488caf9
corrado1982 <104769882+corrado1982@users.noreply.github.com>
2023-01-17 16:05:15 +0100

    fixing the loop

--- script/home-page.js
+++ script/home-page.js
@@ -1,47 +1,65 @@
-import { games } from "./array-games.js";
-console.log("ok");
-const mostSold = document.querySelector(".most-sold-row");
-const hotDeals = document.querySelector(".hot-deals-row");
-const newArrived = document.querySelector(".new-arrived-row");
+// import { games } from "./array-games.js";
+// console.log("ok");
 
-for (let i = 0; i < games.length; i++) {
-  let nameGame = games[i].title;
-  let cover = games[i].image;
-  let priceNew = games[i].newPrice;
-  let priceDigital = games[i].digitalPrice;
-  
+const url = "https://issimo.one/games-hub/wp-json/wc/store/products";
+// const mostSold = document.querySelector(".most-sold-row");
+// const hotDeals = document.querySelector(".hot-deals-row");
+// const newArrived = document.querySelector(".new-arrived-row");
 
-  if (games[i].hotDeals === true) {
-    hotDeals.innerHTML += `<a href="detail.html?id=${games[i].id}" class="cards-background" >
-  <h4 class="title-card">${nameGame}</h4>
-  <img src="${cover}" alt="image of${nameGame}" class="item-img">
-  <p>New: ${priceNew} $</p>
-  <p>Digital: ${priceDigital} $</p>
-  
-  </a>
-  `;
+async function fetchApi() {
+  const response = await fetch(url);
+  const data = await response.json();
+  console.log(data);
+
+  for (let i = 0; i < data.length; i++) {
+    let nameGame = data[i].name;
+    let cover = data[i].images[0];
+
+    console.log(nameGame);
+    console.log(cover);
   }
-  if (games[i].mostSold === true) {
-    mostSold.innerHTML += `<a href="detail.html?id=${games[i].id}" class="cards-background" >
-  <h4 class="title-card">${nameGame}</h4>
-  <img src="${cover}" alt="image of${nameGame}" class="item-img">
-  <p>New: ${priceNew} $</p>
-  <p>Digital: ${priceDigital} $</p>
-  
-  </a>
-  `;
-  }
-  if (games[i].newArrived === true) {
-    newArrived.innerHTML += `<a href="detail.html?id=${games[i].id}" class="cards-background" >
-  <h4 class="title-card">${nameGame}</h4>
-  <img src="${cover}" alt="image of${nameGame}" class="item-img">
-  <p>New: ${priceNew} $</p>
-  <p>Digital: ${priceDigital} $</p>
-  
-  </a>
-  `;
-  }
-  console.log(games[i]);
 }
-console.log("prova");
+
+fetchApi()
+
+
+
+//   let cover = data[i].image;
+//   let priceNew = data[i].newPrice;
+//   let priceDigital = data[i].digitalPrice;
+  
+
+//   if (games[i].hotDeals === true) {
+//     hotDeals.innerHTML += `<a href="detail.html?id=${games[i].id}" class="cards-background" >
+//   <h4 class="title-card">${nameGame}</h4>
+//   <img src="${cover}" alt="image of${nameGame}" class="item-img">
+//   <p>New: ${priceNew} $</p>
+//   <p>Digital: ${priceDigital} $</p>
+  
+//   </a>
+//   `;
+//   }
+//   if (games[i].mostSold === true) {
+//     mostSold.innerHTML += `<a href="detail.html?id=${games[i].id}" class="cards-background" >
+//   <h4 class="title-card">${nameGame}</h4>
+//   <img src="${cover}" alt="image of${nameGame}" class="item-img">
+//   <p>New: ${priceNew} $</p>
+//   <p>Digital: ${priceDigital} $</p>
+  
+//   </a>
+//   `;
+//   }
+//   if (games[i].newArrived === true) {
+//     newArrived.innerHTML += `<a href="detail.html?id=${games[i].id}" class="cards-background" >
+//   <h4 class="title-card">${nameGame}</h4>
+//   <img src="${cover}" alt="image of${nameGame}" class="item-img">
+//   <p>New: ${priceNew} $</p>
+//   <p>Digital: ${priceDigital} $</p>
+  
+//   </a>
+//   `;
+//   }
+//   console.log(games[i]);
+// }
+// console.log("prova");
 
