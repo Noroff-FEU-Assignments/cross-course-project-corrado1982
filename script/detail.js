f2cf7ed27473ec543b26dba8bf243e8e372d605e
corrado1982 <104769882+corrado1982@users.noreply.github.com>
2023-01-18 12:35:31 +0100

    wip

--- script/detail.js
+++ script/detail.js
@@ -1,29 +1,62 @@
-import { games } from "./array-games.js";
+//inizio ad editare la pagina dei dettagli
+//attenzione fermarsi qui
+
+// const url = "https://issimo.one/games-hub/wp-json/wc/store/products" + id ;
+// const mostSold = document.querySelector(".most-sold-row");
+// const hotDeals = document.querySelector(".hot-deals-row");
+// const newArrived = document.querySelector(".new-arrived-row");
+
+
+
+// import { games } from "./array-games.js";
 
 const detailLeft = document.querySelector(".product-page_left");
 const addToCart = document.querySelector("#add-cart_detailpage");
 const breadCrubName = document.querySelector(".this-game");
 
+
+
 const queryString = document.location.search;
 const params = new URLSearchParams(queryString);
 const id = params.get("id");
+console.log(id);
+const url = "https://issimo.one/games-hub/wp-json/wc/store/products/" + id ;
 
-const foundGame = games.find(findGame);
 
-function findGame(foundGame) {
-  if (foundGame.id == id) {
-    return foundGame.id;
-  }
-}
+// const foundGame = data.find(findGame);
+
+// function findGame(foundGame) {
+//   if (foundGame.id == id) {
+//     return foundGame.id;
+//   }
+// }
+async function fetchApi() {
+  const response = await fetch(url);
+  const data = await response.json();
+  console.log(data);
+
+  
+
 // breadCrubName.innerHTML = `${foundGame.title}`;
-detailLeft.innerHTML = `<img class="product-page_image" src="${foundGame.image}" alt="image of${foundGame.title}" class="item-img">
-              <h3>${foundGame.title}</h3>
-              <p class="game-description">${foundGame.description}</p>
+detailLeft.innerHTML = `<img class="product-page_image" src="${data.images[0].src}" alt="image of${data.title}" class="item-img">
+              <h3>${data.name}</h3>
+              <p class="game-description">${data.description}</p>
       `;
-addToCart.innerHTML += `<a href="cart.html?id=${foundGame.id}" class="cta buy-button">Add to cart</a>`;
+addToCart.innerHTML += `<a href="cart.html?id=${data.id}" class="cta buy-button">Add to cart</a>`;
+addToCart.onclick = function () {
+  localStorage.setItem("data", JSON.stringify(data));
+}
+;}
+fetchApi();
+
+
 addToCart.onclick = function () {
   localStorage.setItem("foundGame", JSON.stringify(foundGame));
-};
+}
+
+
+
+
 //try
 // addToCart.forEach((button) => {
 //   button.addEventListener("click", collectGames);
