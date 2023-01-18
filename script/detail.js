f176492249d5fbd1416ec9011ab6e3e1bd3e6a6a
corrado1982 <104769882+corrado1982@users.noreply.github.com>
2023-01-18 14:01:19 +0100

    fixed items
    I left some comment in case I will need to loop inside the loop

--- script/detail.js
+++ script/detail.js
@@ -14,17 +14,13 @@ const detailLeft = document.querySelector(".product-page_left");
 const addToCart = document.querySelector("#add-cart_detailpage");
 const breadCrubName = document.querySelector(".this-game");
 
-
-
 const queryString = document.location.search;
 const params = new URLSearchParams(queryString);
 const id = params.get("id");
 console.log(id);
 const url = "https://issimo.one/games-hub/wp-json/wc/store/products/" + id ;
 
-
 // const foundGame = data.find(findGame);
-
 // function findGame(foundGame) {
 //   if (foundGame.id == id) {
 //     return foundGame.id;
@@ -35,9 +31,7 @@ async function fetchApi() {
   const data = await response.json();
   console.log(data);
 
-  
-
-// breadCrubName.innerHTML = `${foundGame.title}`;
+  // breadCrubName.innerHTML = `${foundGame.title}`;
 detailLeft.innerHTML = `<img class="product-page_image" src="${data.images[0].src}" alt="image of${data.title}" class="item-img">
               <h3>${data.name}</h3>
               <p class="game-description">${data.description}</p>
@@ -49,30 +43,6 @@ addToCart.onclick = function () {
 ;}
 fetchApi();
 
-
 addToCart.onclick = function () {
   localStorage.setItem("foundGame", JSON.stringify(foundGame));
 }
-
-
-
-
-//try
-// addToCart.forEach((button) => {
-//   button.addEventListener("click", collectGames);
-// });
-// function collectGames() {
-//   localStorage.setItem("foundGame", JSON.stringify(foundGame));
-// }
-//TIL HERE
-
-//OLD
-// for (let i = 0; i < games.length; i++) {
-//   if (games[i].id == id) {
-//     detailLeft.innerHTML = `<img class="product-page_image" src="${games[i].image}" alt="image of${games[i].title}" class="item-img">
-//         <h3>${games[i].title}</h3>
-//         <p class="game-description">${games[i].description}</p>
-//         `;
-//     addToCart.innerHTML = `<a href="cart.html?id=${games[i].id}" class="cta buy-button">Add to cart</a>`;
-//   }
-// }
--- script/items.js
+++ script/items.js
@@ -1,198 +1,59 @@
-// const games = [
-//   {
-//     id: 01,
-//     title: "Furious",
-//     image: "images/games img/cover-furious.jpg",
-//     pXboxOne: true,
-//     pXbox: true,
-//     pPs4: true,
-//     pPs5: false,
-//     pNintendo: true,
-//     pPc: true,
-//     newPrice: 16,
-//     usedPrice: 9,
-//     digitalPrice: 10,
-//     description:
-//       "Furious is a game where you can challange your ability alone or with friends.",
-//   },
-//   {
-//     id: 02,
-//     title: "Super Duper",
-//     image: "images/games img/cover-superduper.jpg",
-//     pXboxOne: true,
-//     pXbox: true,
-//     pPs4: true,
-//     pPs5: true,
-//     pNintendo: true,
-//     pPc: true,
-//     newPrice: 16,
-//     usedPrice: 9,
-//     digitalPrice: 10,
-//     description:
-//       "Super Duper is a game where you can challange your ability alone or with friends.",
-//   },
-//   {
-//     id: 03,
-//     title: "Boxer",
-//     image: "images/games img/cover-boxer.jpg",
-//     pXboxOne: true,
-//     pXbox: true,
-//     pPs4: true,
-//     pPs5: true,
-//     pNintendo: true,
-//     pPc: true,
-//     newPrice: 16,
-//     usedPrice: 9,
-//     digitalPrice: 10,
-//     description:
-//       "Boxer is a game where you can challange your ability alone or with friends.",
-//   },
-//   {
-//     id: 04,
-//     title: "Cyberpunk",
-//     image: "images/games img/cover-cyberpunk.jpg",
-//     pXboxOne: true,
-//     pXbox: true,
-//     pPs4: true,
-//     pPs5: true,
-//     pNintendo: true,
-//     pPc: true,
-//     newPrice: 16,
-//     usedPrice: 9,
-//     digitalPrice: 10,
-//     description:
-//       "Cyberpunk is a game where you can challange your ability alone or with friends.",
-//   },
-//   {
-//     id: 05,
-//     title: "Forgelegend",
-//     image: "images/games img/cover-forgelegend.jpg",
-//     pXboxOne: true,
-//     pXbox: true,
-//     pPs4: true,
-//     pPs5: true,
-//     pNintendo: true,
-//     pPc: true,
-//     newPrice: 16,
-//     usedPrice: 9,
-//     digitalPrice: 10,
-//     description:
-//       "Forgelegend is a game where you can challange your ability alone or with friends.",
-//   },
-//   {
-//     id: 06,
-//     title: "Ping Pong",
-//     image: "images/games img/cover-pingpong.jpg",
-//     pXboxOne: true,
-//     pXbox: true,
-//     pPs4: true,
-//     pPs5: true,
-//     pNintendo: true,
-//     pPc: true,
-//     newPrice: 16,
-//     usedPrice: 9,
-//     digitalPrice: 10,
-//     description:
-//       "Ping Pong is a game where you can challange your ability alone or with friends.",
-//   },
-//   {
-//     id: 07,
-//     title: "Racing",
-//     image: "images/games img/cover-racing.jpg",
-//     pXboxOne: true,
-//     pXbox: true,
-//     pPs4: true,
-//     pPs5: true,
-//     pNintendo: true,
-//     pPc: true,
-//     newPrice: 16,
-//     usedPrice: 9,
-//     digitalPrice: 10,
-//     description:
-//       "Racing is a game where you can challange your ability alone or with friends.",
-//   },
-//   {
-//     id: 08,
-//     title: "Space War",
-//     image: "images/games img/cover-spacewar.jpg",
-//     pXboxOne: true,
-//     pXbox: true,
-//     pPs4: true,
-//     pPs5: true,
-//     pNintendo: true,
-//     pPc: true,
-//     newPrice: 16,
-//     usedPrice: 9,
-//     digitalPrice: 10,
-//     description:
-//       "Space War is a game where you can challange your ability alone or with friends.",
-//   },
-//   {
-//     id: 09,
-//     title: "Black",
-//     image: "images/games img/cover-black.jpg",
-//     pXboxOne: true,
-//     pXbox: true,
-//     pPs4: true,
-//     pPs5: true,
-//     pNintendo: true,
-//     pPc: true,
-//     newPrice: 16,
-//     usedPrice: 9,
-//     digitalPrice: 10,
-//     description:
-//       "Black is a game where you can challange your ability alone or with friends.",
-//   },
-//   {
-//     id: 10,
-//     title: "Assassin",
-//     image: "images/games img/cover-assassins.jpg",
-//     pXboxOne: true,
-//     pXbox: true,
-//     pPs4: true,
-//     pPs5: true,
-//     pNintendo: true,
-//     pPc: true,
-//     newPrice: 16,
-//     usedPrice: 9,
-//     digitalPrice: 10,
-//     description:
-//       "Assassin is a game where you can challange your ability alone or with friends.",
-//   },
-// ];
-
-import { games } from "./array-games.js";
+const url = "https://issimo.one/games-hub/wp-json/wc/store/products";
 
 const htmlContainer = document.querySelector(".ps5-page");
 const addCart = document.querySelector("#add-cart");
+
+async function fetchApi() {
+  const response = await fetch(url);
+  const data = await response.json();
+  console.log(data);
+
+  for (let i = 0; i < data.length; i++) {
+    let nameGame = data[i].name;
+    let cover = data[i].images[0].src;
+    let price = data[i].prices.price;
+    
+    
+    // for(let a = 0; a < data[i].tags.length; a++) {
+    // let category = data[i].tags[a].id;
+    // console.log(nameGame);
+    // console.log(cover);
+    // console.log(price);
+    // console.log(category);
+
+    htmlContainer.innerHTML += `<a href="detail.html?id=${data[i].id}" class="cards-background" >
+    <h4 class="title-card">${nameGame}</h4>
+    <img src="${cover}" alt="image of${nameGame}" class="item-img">
+    <p>New: ${price} $</p>
+    <p>Digital: ${price} $</p>
+    </a>
+    `;
+}
+  };
+    // }
+    fetchApi();
+
+
+
 // const detailLeft = document.querySelector(".product-page_left");
 
-for (let i = 0; i < games.length; i++) {
-  let nameGame = games[i].title;
-  let cover = games[i].image;
-  let priceNew = games[i].newPrice;
-  let priceUsed = games[i].usedPrice;
-  let priceDigital = games[i].digitalPrice;
+// for (let i = 0; i < games.length; i++) {
+//   let nameGame = games[i].title;
+//   let cover = games[i].image;
+//   let priceNew = games[i].newPrice;
+//   let priceUsed = games[i].usedPrice;
+//   let priceDigital = games[i].digitalPrice;
   //   let descript = games[i].description;
 
   // if (games[i].pPs5 === false) {
   //   continue;
   // }
 
-  htmlContainer.innerHTML += `<a href="detail.html?id=${games[i].id}" class="cards-background" >
-  <h4 class="title-card">${nameGame}</h4>
-  <img src="${cover}" alt="image of${nameGame}" class="item-img">
-  <p>New: ${priceNew} $</p>
-  <p>Digital: ${priceDigital} $</p>
-  </a>
-  `;
-
-  console.log(games[i]);
-}
-
-// PROVA;
-
-// function insertPicture() {
-//   detailLeft.innerHTML = `<img src="${cover}" alt="image of${nameGame}" class="item-img">`;
+//   htmlContainer.innerHTML += `<a href="detail.html?id=${games[i].id}" class="cards-background" >
+//   <h4 class="title-card">${nameGame}</h4>
+//   <img src="${cover}" alt="image of${nameGame}" class="item-img">
+//   <p>New: ${priceNew} $</p>
+//   <p>Digital: ${priceDigital} $</p>
+//   </a>
+//   `;
 // }
-// insertPicture();
